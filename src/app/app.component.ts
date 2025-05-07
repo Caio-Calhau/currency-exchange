7c31056cae1f721fd87dfea2679c89fed282f466
Caio Martins <caiocalhaum@gmail.com>
2025-05-06 23:50:38 -0300

    fix: styles and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
+import { ActionLabsLogoComponent } from './action-labs-logo/action-labs-logo.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [ActionLabsLogoComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'currency-exchange';
+
+  currencyCode = '';
+
+  submitCurrency() {
+    if (!this.currencyCode.trim()) {
+      alert('Currency must be selected');
+      return;
+    }
+    console.log('sending currency code: ', this.currencyCode);
+  }
 }
