f8226d90d9e8ccd7ddd1bf3553ae71c64c677315
Caio Martins <caiocalhaum@gmail.com>
2025-05-08 01:42:09 -0300

    docs: create environment

--- src/app/api.services.ts
+++ src/app/api.services.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
+  private apiKey = environment.apiKey;
   constructor(private http: HttpClient) {}
 
   getDailyExchangeRate(currencyCode: string): Observable<any> {
     return this.http.get<any>(
-      `https://api-brl-exchange.actionlabs.com.br/api/1.0/open/dailyExchangeRate?apiKey=RVZG0GHEV2KORLNA&from_symbol=${currencyCode}&to_symbol=BRL`
+      `https://api-brl-exchange.actionlabs.com.br/api/1.0/open/dailyExchangeRate?apiKey=${this.apiKey}&from_symbol=${currencyCode}&to_symbol=BRL`
     );
   }
   getCurrentExchangeRate(currencyCode: string): Observable<any> {
     return this.http.get(
-      `https://api-brl-exchange.actionlabs.com.br/api/1.0/open/currentExchangeRate?apiKey=RVZG0GHEV2KORLNA&from_symbol=${currencyCode}&to_symbol=BRL`
+      `https://api-brl-exchange.actionlabs.com.br/api/1.0/open/currentExchangeRate?apiKey=${this.apiKey}&from_symbol=${currencyCode}&to_symbol=BRL`
     );
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: 'RVZG0GHEV2KORLNA',
+};
